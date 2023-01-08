0f03e0f77934d42161602885bb8d7bb11f5a9da6
Kirthika <kirthijr@gmail.com>
2023-01-08 23:53:30 +0530

    Update LongestPalindrome.java

--- LongestPalindrome.java
+++ LongestPalindrome.java
@@ -1,9 +1,6 @@
-// Given a string s, return the longest 
-// palindromic
- 
-// substring
-//  in s.
-//   Input: s = "babad"
+// Given a string s, return the longest palindromic substring in s.
+
+// Input: s = "babad"
 // Output: "bab"
 // Explanation: "aba" is also a valid answer.
 
