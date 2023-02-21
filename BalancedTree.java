5394a2994061c142fa047c57c10233510514352f
Kirthika <kirthijr@gmail.com>
2023-02-21 22:43:29 +0530

    Height Balanced Tree

---
+++ BalancedTree.java
@@ -0,0 +1,21 @@
+// Given a binary tree, determine if it is 
+// height-balanced
+// .
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null)
+            return true;
+        else if (Math.abs(height(root.left) - height(root.right)) >1)
+            return false;
+        else
+            return isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    public int height(TreeNode root){
+        if(root == null)
+            return 0;
+        int lheight = height(root.left);
+        int rheight = height(root.right);
+        return Math.max(lheight,rheight)+1;
+    }
+}
