570539d84ada34aad8d474ae1e8ba1f239b3f4ba
Kirthika <kirthijr@gmail.com>
2023-02-27 16:48:10 +0530

    Longest Palindrome for normal and jumbled strings

--- LongestPalindrome.java
+++ LongestPalindrome.java
@@ -29,3 +29,31 @@ class LongestPalindrome {
     }
     
 }
+/*
+Given a string s which consists of lowercase or uppercase letters, return the length 
+of the longest palindrome that can be built with those letters.
+Input: s = "abccccdd"
+Output: 7
+Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+*/
+
+Letters are case sensitive, for example, "Aa" is not considered a palindrome here.
+
+
+class Solution {
+    public int longestPalindrome(String s) {
+        int result=0;
+        int[] arr=new int[128];
+        for(char c:s.toCharArray()){
+            arr[c]++;
+        }
+
+        for(int val: arr){
+            result += val/2 * 2;
+
+            if(result % 2 == 0 && val % 2 == 1)
+                result++;
+        }
+        return result;
+    }
+}
